129f16cd7438c65546714f61d2f126bde04ba753
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-01-02 19:11:02 +0100

    CRACEN RNG HAL: Remove senseless static inline
    That was just a copy paste mistake
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- src/nrfx/hal/nrf_cracen_rng.c
+++ src/nrfx/hal/nrf_cracen_rng.c
@@ -11,8 +11,8 @@
 #include "bs_tracing.h"
 #include "NHW_CRACEN_RNG.h"
 
-NRF_STATIC_INLINE void nrf_cracen_rng_control_set(NRF_CRACENCORE_Type *      p_reg,
-                                                  nrf_cracen_rng_control_t * p_config)
+void nrf_cracen_rng_control_set(NRF_CRACENCORE_Type *      p_reg,
+                                nrf_cracen_rng_control_t * p_config)
 {
     p_reg->RNGCONTROL.CONTROL =
           ((p_config->enable << CRACENCORE_RNGCONTROL_CONTROL_ENABLE_Pos)
@@ -26,7 +26,7 @@ NRF_STATIC_INLINE void nrf_cracen_rng_control_set(NRF_CRACENCORE_Type *      p_r
     nhw_CRACEN_RNG_regw_sideeffects_CONTROL();
 }
 
-NRF_STATIC_INLINE uint32_t nrf_cracen_rng_fifo_get(NRF_CRACENCORE_Type const * p_reg)
+uint32_t nrf_cracen_rng_fifo_get(NRF_CRACENCORE_Type const * p_reg)
 {
     nhw_CRACEN_RNG_regr_sideeffects_FIFO();
     return p_reg->RNGCONTROL.FIFO[0];
