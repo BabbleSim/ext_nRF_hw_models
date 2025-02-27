9fd413337a70ecc2e81391e51b8aa5ba5570984a
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-02-27 14:22:02 +0100

    MDK nrfx_erratas.h: Ensure backwards compatibility with old nrfx version
    Older nrfx code needs to link to some erratas.
    So let's provide a valid version for those to avoid breaking that SW.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- src/nrfx/mdk_replacements/nrf_erratas.h
+++ src/nrfx/mdk_replacements/nrf_erratas.h
@@ -6,12 +6,23 @@
 
 /**
  * We do not have any errata we want to run in simulation yet,
- * so we do not define any NRF<xxx>_ERRATA_<yy>_ENABLE_WORKAROUND or nrf<xxx>_errata_<yy>
+ * so in general we do not define any NRF<xxx>_ERRATA_<yy>_ENABLE_WORKAROUND or nrf<xxx>_errata_<yy>
  */
 
 #ifndef NRF_ERRATAS_H
 #define NRF_ERRATAS_H
 
+#include <stdbool.h>
 #include "nrf.h"
+
+/* Users of nrf_errata.h should guard calls to nrf<xxx>_errata_<yy> with an #ifdef NRF<xxx>_ERRATA_<yy>_ENABLE_WORKAROUND
+ * But in some places this was not done, so let's provide a working replacement for those to avoid breaking that SW */
+#define _BSIM_NRF_N_ERRATA_M(n,m) static inline bool nrf##n##_errata_##m(void) { return false; }
+
+_BSIM_NRF_N_ERRATA_M(52,122)
+_BSIM_NRF_N_ERRATA_M(53,4)
+_BSIM_NRF_N_ERRATA_M(53,161)
+
+#undef _BSIM_NRF_N_ERRATA_M
 
 #endif // NRF_ERRATAS_H
