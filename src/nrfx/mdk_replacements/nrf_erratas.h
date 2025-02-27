85061b13ef825ad062fb34270a2aaaaad0ca1a28
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-02-27 10:42:12 +0100

    MDK: Provide empty replacement for nrf_erratas.h
    This will ensure no erratas, which cannot be run in simulation, are run.
    But, users must follow the correct way of using this header by checking
    at build time if NRF<xxx>_ERRATA_<yy>_ENABLE_WORKAROUND is defined
    before calling nrf<xxx>_errata_<yy>().
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

---
+++ src/nrfx/mdk_replacements/nrf_erratas.h
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * We do not have any errata we want to run in simulation yet,
+ * so we do not define any NRF<xxx>_ERRATA_<yy>_ENABLE_WORKAROUND or nrf<xxx>_errata_<yy>
+ */
+
+#ifndef NRF_ERRATAS_H
+#define NRF_ERRATAS_H
+
+#include "nrf.h"
+
+#endif // NRF_ERRATAS_H
