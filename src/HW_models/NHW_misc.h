9f11c2f673c7ebeba1dde4b1e996e4667db8650d
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-12-02 17:35:31 +0100

    NHW_misc: Add API to convert from/to simulated addr to real HW ones
    To be able to better support SW which otherwise tries to act on
    real HW addresses.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- src/HW_models/NHW_misc.h
+++ src/HW_models/NHW_misc.h
@@ -16,6 +16,10 @@ extern "C" {
 const char *nhw_get_core_name(unsigned int core_n);
 void *nhw_convert_RAM_addr(void *ram_addr);
 
+void *nhw_convert_per_addr_sim_to_hw(void *sim_addr);
+void *nhw_convert_per_addr_hw_to_sim(void *real_addr);
+void *nhw_convert_periph_base_addr(void *hw_addr);
+
 #ifdef __cplusplus
 }
 #endif
