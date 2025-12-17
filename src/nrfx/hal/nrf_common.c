3824f9c8859eebde721c1098119b97bff570bbb8
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-12-17 14:33:14 +0100

    hal: nrf_common: Add replacement for nrf_address_{bus,slave}_get()
    Add a replacement for simulation which takes a simulated peripheral
    addresses and returns the corresponding bus/slave.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- src/nrfx/hal/nrf_common.c
+++ src/nrfx/hal/nrf_common.c
@@ -7,6 +7,25 @@
  */
 
 #include <stdbool.h>
+#include <stdint.h>
+#include <nrfx.h>
+#include "NHW_misc.h"
+
+#if defined(ADDRESS_BUS_Msk)
+uint8_t nrf_address_bus_get(uint32_t addr, size_t size)
+{
+  uintptr_t real_hw_addr = (uintptr_t)nhw_convert_per_addr_sim_to_hw((void *)addr);
+  return (uint8_t)((real_hw_addr & ADDRESS_BUS_Msk & ~(size - 1)) >> ADDRESS_BUS_Pos);
+}
+#endif
+
+#if defined(ADDRESS_SLAVE_Msk)
+uint8_t nrf_address_slave_get(uint32_t addr)
+{
+  uintptr_t real_hw_addr = (uintptr_t)nhw_convert_per_addr_sim_to_hw((void *)addr);
+  return (uint8_t)((real_hw_addr & ADDRESS_SLAVE_Msk) >> ADDRESS_SLAVE_Pos);
+}
+#endif
 
 bool nrf_dma_accessible_check(void const * p_reg, void const * p_object)
 {
